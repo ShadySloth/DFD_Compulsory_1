761a4f6f8b46c4716181c86a4fbc93585ba49236
ShadySloth <pedant01@easv365.dk>
2025-02-24 11:14:43 +0100

    chore: readme update

--- DFD_Compulsory_1/Entities/Student.cs
+++ DFD_Compulsory_1/Entities/Student.cs
@@ -7,13 +7,13 @@ public class Student
     public int Id { get; set; }
     
     [Column(TypeName = "varchar(50)")]
-    public string FirstName { get; set; }
+    public required string FirstName { get; set; }
     
     [Column(TypeName = "varchar(50)")]
-    public string LastName { get; set; }
+    public required string LastName { get; set; }
     
     [Column(TypeName = "varchar(60)")]
-    public string Email { get; set; }
+    public required string Email { get; set; }
     
     public DateTime EnrollmentDate { get; set; }
 }
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Compolsory 1 for the subject, Database for Developers. 
 
 There are two main off-branches, State and Change. They each contain database migrations for each method of doing migrations.
+
+### Entities
+
+In our student and course entity we made sure to use the column typeName annotation to ensure that the strings of the entities has a character limit.
+
+In our enrollment entity we created a enum for the grades to ensure that only the correct values could be set as a grade.
