78504288b5d7723fb68346e3397df9b2ff26aea4
ShadySloth <pedant01@easv365.dk>
2025-02-24 11:35:19 +0100

    chore: added context

---
+++ DFD_Compulsory_1/Entities/SchoolContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DFD_Compulsory_1.Entities;
+
+public class SchoolContext : DbContext
+{
+    protected SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Enrollment> Enrollments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Enrollment>()
+            .HasOne(enrollment => enrollment.Student)
+            .WithMany()
+            .HasForeignKey(enrollment => enrollment.StudentId);
+        
+        modelBuilder.Entity<Enrollment>()
+            .HasOne(enrollment => enrollment.Course)
+            .WithMany()
+            .HasForeignKey(enrollment => enrollment.CourseId);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
