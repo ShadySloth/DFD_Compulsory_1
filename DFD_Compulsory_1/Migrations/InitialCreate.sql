eb27db6f95525ecdc0a7ac5bbbacfb83ad901e29
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2025-03-03 09:24:40 +0100

    chore: SQL for first assignment

---
+++ DFD_Compulsory_1/Migrations/InitialCreate.sql
@@ -0,0 +1,82 @@
+IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+CREATE TABLE [__EFMigrationsHistory] (
+    [MigrationId] nvarchar(150) NOT NULL,
+    [ProductVersion] nvarchar(32) NOT NULL,
+    CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250303080425_InitialCreate'
+)
+BEGIN
+CREATE TABLE [Courses] (
+    [Id] int NOT NULL IDENTITY,
+    [Title] varchar(50) NOT NULL,
+    [Credits] int NOT NULL,
+    CONSTRAINT [PK_Courses] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250303080425_InitialCreate'
+)
+BEGIN
+CREATE TABLE [Students] (
+    [Id] int NOT NULL IDENTITY,
+    [FirstName] varchar(50) NOT NULL,
+    [LastName] varchar(50) NOT NULL,
+    [Email] varchar(60) NOT NULL,
+    [EnrollmentDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Students] PRIMARY KEY ([Id])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250303080425_InitialCreate'
+)
+BEGIN
+CREATE TABLE [Enrollments] (
+    [Id] int NOT NULL IDENTITY,
+    [StudentId] int NOT NULL,
+    [CourseId] int NOT NULL,
+    [Grade] int NOT NULL,
+     CONSTRAINT [PK_Enrollments] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Enrollments_Courses_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Courses] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Enrollments_Students_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [Students] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250303080425_InitialCreate'
+)
+BEGIN
+CREATE INDEX [IX_Enrollments_CourseId] ON [Enrollments] ([CourseId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250303080425_InitialCreate'
+)
+BEGIN
+CREATE INDEX [IX_Enrollments_StudentId] ON [Enrollments] ([StudentId]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250303080425_InitialCreate'
+)
+BEGIN
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250303080425_InitialCreate', N'9.0.2');
+END;
+
+COMMIT;
+GO
