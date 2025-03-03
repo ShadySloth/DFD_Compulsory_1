7a8c3baa8458bfe88a344ed710310dae3a0d6518
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2025-03-03 10:29:57 +0100

    chore: Created SQL files for migrations

---
+++ DFD_Compulsory_1/Migrations/AddDepartment.sql
@@ -0,0 +1,19 @@
+﻿BEGIN TRANSACTION;
+CREATE TABLE [Department] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] varchar(50) NOT NULL,
+    [Budget] decimal(18,2) NOT NULL,
+    [StartDate] datetime2 NOT NULL,
+    [InstructorId] int NOT NULL,
+    CONSTRAINT [PK_Department] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Department_Instructors_InstructorId] FOREIGN KEY ([InstructorId]) REFERENCES [Instructors] ([Id]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_Department_InstructorId] ON [Department] ([InstructorId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250303085324_AddDepartments', N'9.0.2');
+
+COMMIT;
+GO
+
---
+++ DFD_Compulsory_1/Migrations/AddInstructor.sql
@@ -0,0 +1,21 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Courses] ADD [InstructorId] int NOT NULL DEFAULT 0;
+
+CREATE TABLE [Instructors] (
+    [Id] int NOT NULL IDENTITY,
+    [FirstName] varchar(50) NOT NULL,
+    [LastName] varchar(50) NOT NULL,
+    [HireDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Instructors] PRIMARY KEY ([Id])
+);
+
+CREATE INDEX [IX_Courses_InstructorId] ON [Courses] ([InstructorId]);
+
+ALTER TABLE [Courses] ADD CONSTRAINT [FK_Courses_Instructors_InstructorId] FOREIGN KEY ([InstructorId]) REFERENCES [Instructors] ([Id]) ON DELETE CASCADE;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250224115648_AddInstructor', N'9.0.2');
+
+COMMIT;
+GO
+
